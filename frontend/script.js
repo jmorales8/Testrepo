23d3993a393314b0dafd954ebfa49aaec20f0554
jmoral8 <jaimmoral98@gmail.com>
2025-07-24 12:40:47 -0500

    poke api stuff

--- frontend/script.js
+++ frontend/script.js
@@ -2,18 +2,45 @@
 async function fetchData() {
   try {
     // catch the response as data, not it isnt in json form yet
-    const response = await fetch('http://localhost:3000/bruh');
-    console.log("this is a response: ", response)
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon/ditto');
+    // console.log("this is a response: ", response)
 
     // if this console.log is uncomented, then it will reult in an error because 
-    // console.log("this is a promise that wont be fulffiled because it wont the request wont have time to finish:", response.json())
+    // console.log("this is a promise that wont be fulffiled because it wont the request wont have time to finish:", await response.json())
     if (!response.ok) throw new Error('Network response was not OK');
     const data = await response.json();
     // this logs the data in json format
-    console.log(data);
+    // fetchURL(data.abilities);
+    console.log("this is data", data)
+    console.log("this is data.abilities", data.abilities)
+    console.log("this is data.abilities[0]", data.abilities[0])
+    console.log("this is data.abilities[0].ability", data.abilities[0].ability)
     //we pass the entire json object to the dom without parsing it
-    document.getElementById('output').textContent = JSON.stringify(data, null, 2);
+    document.getElementById('output').textContent = JSON.stringify(data.held_items[1].item.url, null, 2);
   } catch (error) {
     console.error('Fetch error:', error);
   }
 }
+
+
+// fetch(url).then(dosomething).catch(error)
+// async function fetchURL(ability) {
+//     try {
+//     // catch the response as data, not it isnt in json form yet
+//     const response = await fetch('https://pokeapi.co/api/v2/pokemon/ditto');
+//     console.log("this is a response: ", response)
+
+//     // if this console.log is uncomented, then it will reult in an error because 
+//     // console.log("this is a promise that wont be fulffiled because it wont the request wont have time to finish:", response.json())
+//     if (!response.ok) throw new Error('Network response was not OK');
+//     const data = await response.json();
+//     // this logs the data in json format
+//     console.log(data.abilities);
+//     ability = data.abilities
+//     console.log("ability", ability)
+//     //we pass the entire json object to the dom without parsing it
+//     document.getElementById('output').textContent = JSON.stringify(data.abilities, null, 2);
+//   } catch (error) {
+//     console.error('Fetch error:', error);
+//   }
+// }
